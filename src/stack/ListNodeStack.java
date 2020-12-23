985d7a6660a4b5da08bcf9e66e7a69a63b7d25d1
hanull <dkfghg3@naver.com>
2020-12-23 23:28:07 +0900

    ListNodeStack 구현 및 테스트 코드 작성

--- src/stack/ListNodeStack.java
+++ src/stack/ListNodeStack.java
@@ -1,14 +1,48 @@
 package stack;
 
+import linkedlist.ListNode;
+
+import java.util.EmptyStackException;
+
 public class ListNodeStack implements Stack {
+
+    private ListNode stack;
+    private ListNode head;
+    private int size = 0;
 
     @Override
     public void push(int data) {
+        if (isEmpty()) {
+            stack = new ListNode(data);
+            head = stack;
+        } else {
+            ListNode newNode = new ListNode(data);
+            newNode.pre = head;
+            head.next = newNode;
+            head = newNode;
+        }
+        size++;
+    }
 
+    private boolean isEmpty() {
+        return size == 0;
     }
 
     @Override
     public int pop() {
-        return 0;
+        if (isEmpty()) throw new EmptyStackException();
+        int data = head.getData();
+        head = head.pre;
+        size--;
+        return data;
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new EmptyStackException();
+        return head.getData();
+    }
+
+    public int getSize() {
+        return size;
     }
 }
--- tests/stack/ListNodeStackTest.java
+++ tests/stack/ListNodeStackTest.java
@@ -1,7 +1,66 @@
 package stack;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("ListNodeStack 테스트")
 class ListNodeStackTest {
 
+    @Nested
+    @DisplayName("push 메소드는")
+    class Push {
+
+        @Test
+        @DisplayName("데이터를 push한다.")
+        public void push() {
+            ListNodeStack stack = new ListNodeStack();
+
+            stack.push(1);
+            stack.push(2);
+            assertEquals(2, stack.getSize());
+            assertEquals(2, stack.peek());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("pop 메소드는")
+    class Pop {
+
+        @Nested
+        @DisplayName("스택이 비었을 때,")
+        class EmptyStack {
+            @DisplayName("예외를 발생시킨다.")
+            @Test
+            public void throwException() {
+                ListNodeStack stack = new ListNodeStack();
+
+                stack.push(1);
+                stack.pop();
+                assertThrows(EmptyStackException.class, () -> stack.pop());
+            }
+        }
+
+        @Nested
+        @DisplayName("스택에 데이터가 있을 때,")
+        class HasData {
+            @DisplayName("가장 위에 있는 데이터를 반환한다.")
+            @Test
+            public void popData() {
+                ListNodeStack stack = new ListNodeStack();
+
+                stack.push(1);
+                stack.push(2);
+                int data = stack.pop();
+
+                assertEquals(data, 2);
+                assertEquals(stack.peek(), 1);
+            }
+        }
+    }
 }
